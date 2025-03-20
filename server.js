97b7a97cf00632ef2af59c47e683b7de04edafae
Marcusantana <165960762+Marcusantana@users.noreply.github.com>
2025-03-20 11:48:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,18 +23,18 @@ app.post('/webhook', (req, res) => {
         var valor = 0;
 
         if (userQuery === ("mayones")) {  //MAYONES
-            valor = 8499.99;
+            valor = 7921.99;
             responseText = `Ótimo! Como você não especificou o modelo, o seu instrumento, as guitarras da ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
 
         else if (userQuery.includes("standard"))  {  
-            valor = 8499.99;
+            valor = 7921.99;
             responseText = `Lindo! As guitarras da ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
             
 
         else if (userQuery.includes("qatsi 7")||userQuery.includes("hydra")) {  
-            valor = 23999.99;
+            valor = 21157.99;
             responseText = `Boa escolha! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
 
@@ -44,36 +44,69 @@ app.post('/webhook', (req, res) => {
         }
 
         else if (userQuery.includes("v2")) {  
-            valor = 39999.99;
+            valor = 35999.99;
             responseText = `Ótimo! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
 
         else if (userQuery.includes("custom shop 7")||userQuery.includes("7 custom shop")) {  
-            valor = 49999.99;
+            valor = 38999.99;
             responseText = `Sábia escolha! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
             
         else if (userQuery.includes("7")) {  
-            valor = 28999.99;
+            valor = 19999.99;
             responseText = `Ótimo! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
 
         else if (userQuery.includes("dirty")) {  
-            valor = 32999.99;
+            valor = 26505.99;
             responseText = `Ótimo! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
 
         else if (userQuery.includes("custom shop")) {  
-            valor = 49999.99;
+            valor = 38999.99;
             responseText = `A melhorzinha do momento! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
         
         else if (userQuery.includes("qatsi") || userQuery.includes("duvell") || userQuery.includes("regius")||userQuery.includes("aquila")) {  
-            valor = 20999.99;
+            valor = 14544.99;
             responseText = `Aí sim! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
         }
-    
 
+
+
+
+        if (userQuery === ("strandberg")) {  //STRANDBERG
+            valor = 8614.99;
+            responseText = `Ótimo! Como você não especificou o modelo, o seu instrumento, as guitarras da ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
+        }
+
+        else if (userQuery.includes("boden standard"))  {  
+            valor = 16561.99;
+            responseText = `Lindo! As guitarras da ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
+        }
+            
+        else if (userQuery.includes("boden essencial")) {  
+            valor = 8614.99;
+            responseText = `Boa escolha! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
+        }
+
+        else if (userQuery.includes("boden metal")||userQuery.includes("boden original")) {  
+            valor = 18584.99;
+            responseText = `Perfeito! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
+        }
+
+        else if (userQuery.includes("boden j artisan")|| userQuery.includes("boden artisan")) {  
+            valor = 79519.99;
+            responseText = `Ótimo! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
+        }
+
+        else if (userQuery.includes("boden true temperament")) {  
+            valor = 33134.99;
+            responseText = `Sábia escolha! As guitarras ${userQuery.toUpperCase()} começam com o valor de: ${formatarMoeda(valor)} \nOs valores dos instrumentos estão sujeitos a alteração com os impostos de importação e as mudanças e upgrades no instrumento (tanto standard e os CUSTOM SHOP).`;
+        }
+            
+    
         
     } else if (intent === 'Despedida') {
         responseText = 'Até logo!';
